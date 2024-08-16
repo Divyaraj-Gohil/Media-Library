96392271d8ad94159447b780e36e078f069b4778
Divyaraj <147273995+Divyaraj-Gohil@users.noreply.github.com>
2024-08-16 14:30:40 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/MERN-image-upload/",
+  base: "/Media-Library/",
   plugins: [react()],
 })
